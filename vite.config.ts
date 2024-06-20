66ffc0a9c608093c6bd9de84e983fdead85a23ae
MikusR <mikus.rakis@gmail.com>
2024-06-20 14:59:00 +0300

    add git commit and date

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as child from "child_process";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+const commitHash = child.execSync('git log -1 --pretty=\'format:%h %ad\' --date=iso8601 HEAD').toString();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        __COMMIT_HASH__: JSON.stringify(commitHash),
+    },
     plugins: [react()],
     base: "/tlgywen/"
 })
